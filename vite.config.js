d7737bdc05507e9e54ed8c0bbf09a0ce939228c8
Tanya Denise Yambao <tdy.alhassan@gmail.com>
2026-07-20 18:45:49 +0800

    fixes for deployment

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
+import path from 'path';
 
 export default defineConfig({
     plugins: [
@@ -15,4 +16,10 @@ export default defineConfig({
         react(),
         tailwindcss(),
     ],
+    resolve: {
+        alias: {
+            '@': path.resolve(__dirname, 'resources/js'),
+            '@layouts': path.resolve(__dirname, 'Layouts'),
+        },
+    },
 });
